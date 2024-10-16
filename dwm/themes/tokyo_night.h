ba409c6d97e571950f02feaeac359f6e8e98ff12
Jakub Skowron <jakubskowron676@gmail.com>
2024-10-16 23:44:18 +0200

    Initial commit

---
+++ dwm/themes/tokyo_night.h
@@ -0,0 +1,6 @@
+static const char background[]       = "#1a1b26";
+static const char foreground[]       = "#f8f8f2";
+static const char white[]            = "#aaaaaa";
+static const char black[] 			 = "#000000";
+//static const char border[]           = "#7aa2f7";
+static const char border[] 			 = "#9ece6a";
---
+++ dwmblocks-async/modules/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo Copying modules to /usr/local/bin...
+
+sudo cp ~/.files/suckless/dwmblocks-async/modules/dba-* /usr/local/bin
