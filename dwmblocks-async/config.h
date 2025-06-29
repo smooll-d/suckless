e33e3fdede8b523766503e45353a27646d96ad96
Jakub Skowron <jakubskowron676@gmail.com>
2025-06-29 20:45:00 +0200

    fix: disable clickable blocks
    dwmblocks didn't play right with my configuration and the
    CLICKABLE_BLOCKS, so I disabled that, separated dba-date into two
    scripts and now it works like it always did :)

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -5,10 +5,10 @@
 #define DELIMITER ""
 
 // Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 45
+#define MAX_BLOCK_OUTPUT_LENGTH 1000
 
 // Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+#define CLICKABLE_BLOCKS 0
 
 // Control whether a leading delimiter should be prepended to the status.
 #define LEADING_DELIMITER 0
@@ -18,8 +18,9 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "dba-date", 60, 3) \
-    X("", "dba-battery", 60, 4) \
+    X("", "dba-date", 0, 1) \
+    X("", "dba-time", 60, 2) \
+    X("", "dba-battery", 60, 3) \
     /* X("", "dba-updates", 3600, 1)  \*/
     /* X("", "dba-disksize", 60, 2)   \*/
 
